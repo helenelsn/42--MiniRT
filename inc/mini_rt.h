c72f5e8e49001ef83907ad077c455d8bd1248198
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-10-18 17:03:22 +0200

    init

---
+++ inc/mini_rt.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_rt.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 15:55:13 by srapin            #+#    #+#             */
+/*   Updated: 2023/10/18 15:59:05 by srapin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_RT_H
+#define MINI_RT_H
+
+# include "../libs/libft/ft_printf.h"
+# include "../libs/minilibx-linux/mlx.h"
+# include "foo.h"
+# include "struct.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+#endif
