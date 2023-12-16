14993aa8d641c8268a1ae4eeef06d854782622f5
Helene Lesny <hlesny@student.42.fr>
2023-12-17 00:20:43 +0100

    redrawing ok with multithreading

--- src/mlx_gestion/redraw.c
+++ src/mlx_gestion/redraw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redraw.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 18:23:11 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/16 21:36:52 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 00:18:31 by hlesny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void redraw(t_app *app)
 {
-    draw_scene(app);
+    // draw_scene(app);
+    redraw_bonus(app);
+
 	mlx_put_image_to_window(app->mlx_data.mlx_ptr, app->mlx_data.win_ptr,
         app->mlx_data.image.img, 0, 0);
     // printf("redr\n", __func__);
