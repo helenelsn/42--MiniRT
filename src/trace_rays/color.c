9f961236025b58b95e46279fe7ed0b0bd2260cc2
Helene Lesny <hlesny@student.42.fr>
2023-12-16 01:37:58 +0100

    cleaned color.c

--- src/trace_rays/color.c
+++ src/trace_rays/color.c
@@ -6,23 +6,12 @@
 /*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:16:50 by hlesny            #+#    #+#             */
-/*   Updated: 2023/12/13 17:50:29 by hlesny           ###   ########.fr       */
+/*   Updated: 2023/12/16 01:35:14 by hlesny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/struct.h"
 #include "../../inc/mini_rt.h"
-
-unsigned char   rgb_float_to_char(float color)
-{
-    return ((char)(color * 255.f));
-}   
-
-float  rgb_char_to_float(int color_comp)
-{
-    return (color_comp / 255.f);
-}
-
 
 int     color_clamp(long color, int min, int max)
 {
@@ -50,18 +39,6 @@ t_color    color_scale(t_color color, double scale)
     if (inter > 255)
         inter = 255;
     color.b = inter;
-
-    // int composant;
-
-    // composant = (color.r * scale);// / 255;
-    // color.r = color_clamp(composant, 0, UCHAR_MAX);
-
-    // composant = (color.g * scale);// / 255;
-    // color.g = color_clamp(composant, 0, UCHAR_MAX);
-
-    // composant = (color.b * scale);// / 255;
-    // color.b = color_clamp(composant, 0, UCHAR_MAX);
-
     return (color);
 }
 
@@ -70,15 +47,10 @@ t_color color_mult(t_color c1, t_color c2)
     int color;
 
     color = (float)(c1.r * c2.r) / 255.f;
-    // color = rgb_float_to_char(rgb_char_to_float(c1.r) * rgb_char_to_float(c2.r));
     c1.r = color_clamp(color, 0, UCHAR_MAX);
-    
     color = (float)(c1.g * c2.g) / 255.f;
-    // color = rgb_float_to_char(rgb_char_to_float(c1.g) * rgb_char_to_float(c2.g));
     c1.g = color_clamp(color, 0, UCHAR_MAX);
-
     color = (float)(c1.b * c2.b) / 255.f;
-    // color = rgb_float_to_char(rgb_char_to_float(c1.b) * rgb_char_to_float(c2.b));
     c1.b = color_clamp(color, 0, UCHAR_MAX);
     return (c1);
 
@@ -91,9 +63,5 @@ t_color    color_add(t_color c1, t_color c2)
     c1.r = color_clamp(c1.r + c2.r, 0, UCHAR_MAX);
     c1.g = color_clamp(c1.g + c2.g, 0, UCHAR_MAX);
     c1.b = color_clamp(c1.b + c2.b, 0, UCHAR_MAX);
-    
-    // c1.r += c2.r;
-    // c1.g += c2.g;
-    // c1.b += c2.b;
     return (c1);
 }
