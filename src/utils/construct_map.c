17e6dd45a958ba11edf7a5cd5bd5a7bb883f2629
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-12-17 03:55:55 +0100

    guess what

--- src/utils/construct_map.c
+++ src/utils/construct_map.c
@@ -6,36 +6,51 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 23:26:42 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/17 01:46:51 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 03:55:35 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/mini_rt.h"
 
-bool construct_map(t_normal_map *map, char *name)
+int	init_map(t_normal_map *map, char *name)
 {
-	int fd = open(name, O_RDONLY);
+	int		fd;
+	char	*line;
+	char	**sp_line;
+
+	fd = open(name, O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Error while constructing map %s\n", name);
-		return (false);
+		return (fd);
 	}
-	char *line;
-	char **sp_line;
 	line = get_next_line(fd);
 	sp_line = ft_split(line, ' ');
 	map->width = atof(sp_line[0]);
-	map->height =atof( sp_line[1]);
+	map->height = atof(sp_line[1]);
 	free_tab(sp_line);
 	free(line);
 	map->pixels = ft_calloc(map->height * map->height, sizeof(t_vec));
+	return (fd);
+}
+
+bool	construct_map(t_normal_map *map, char *name)
+{
+	int		fd;
+	int		i;
+	char	*line;
+	char	**sp_line;
+	t_vec	tmp;
+
+	fd = init_map(map, name);
+	if (fd == -1)
+		return (false);
 	line = get_next_line(fd);
-	int i = 0;
-	while(line && line[0] !='\n')
-	{		
+	i = 0;
+	while (line && line[0] != '\n')
+	{
 		sp_line = ft_split(line, ' ');
-		t_vec tmp;
-		tmp.x = atof( sp_line[0]);
+		tmp.x = atof(sp_line[0]);
 		tmp.y = atof(sp_line[1]);
 		tmp.z = atof(sp_line[2]);
 		map->pixels[i] = tmp;
@@ -45,4 +60,34 @@ bool construct_map(t_normal_map *map, char *name)
 		line = get_next_line(fd);
 	}
 	return (true);
+}
+
+t_normal_map	*get_map_from_type(t_normap_type t)
+{
+	if (t == cliff)
+		return (get_cliff_map(false));
+	if (t == sand)
+		return (get_sand_map(false));
+	if (t == terrain)
+		return (get_terrain_map(false));
+	if (t == wood)
+		return (get_wood_map(false));
+	if (t == tree)
+		return (get_tree_map(false));
+	return (NULL);
+}
+
+t_normal_map	*erase_maps(t_normap_type t)
+{
+	if (t == cliff)
+		return (get_cliff_map(true));
+	if (t == sand)
+		return (get_sand_map(true));
+	if (t == terrain)
+		return (get_terrain_map(true));
+	if (t == wood)
+		return (get_wood_map(true));
+	if (t == tree)
+		return (get_tree_map(true));
+	return (NULL);
 }
--- src/utils/normaps_getter.c
+++ src/utils/normaps_getter.c
@@ -6,13 +6,13 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 01:30:27 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/17 02:46:35 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 03:54:36 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/mini_rt.h"
 
-static t_normal_map	*get_cliff_map(bool to_free)
+t_normal_map	*get_cliff_map(bool to_free)
 {
 	static t_normal_map	*map;
 	static bool			flag;
@@ -41,7 +41,7 @@ static t_normal_map	*get_cliff_map(bool to_free)
 	return (map);
 }
 
-static t_normal_map	*get_sand_map(bool to_free)
+t_normal_map	*get_sand_map(bool to_free)
 {
 	static t_normal_map	*map;
 	static bool			flag;
@@ -70,7 +70,7 @@ static t_normal_map	*get_sand_map(bool to_free)
 	return (map);
 }
 
-static t_normal_map	*get_terrain_map(bool to_free)
+t_normal_map	*get_terrain_map(bool to_free)
 {
 	static t_normal_map	*map;
 	static bool			flag;
@@ -99,7 +99,7 @@ static t_normal_map	*get_terrain_map(bool to_free)
 	return (map);
 }
 
-static t_normal_map	*get_tree_map(bool to_free)
+t_normal_map	*get_tree_map(bool to_free)
 {
 	static t_normal_map	*map;
 	static bool			flag;
@@ -128,7 +128,7 @@ static t_normal_map	*get_tree_map(bool to_free)
 	return (map);
 }
 
-static t_normal_map	*get_wood_map(bool to_free)
+t_normal_map	*get_wood_map(bool to_free)
 {
 	static t_normal_map	*map;
 	static bool			flag;
@@ -155,34 +155,4 @@ static t_normal_map	*get_wood_map(bool to_free)
 	if (!flag)
 		return (NULL);
 	return (map);
-}
-
-t_normal_map	*get_map_from_type(t_normap_type t)
-{
-	if (t == cliff)
-		return (get_cliff_map(false));
-	if (t == sand)
-		return get_sand_map(false);
-	if (t == terrain)
-		return get_terrain_map(false);
-	if (t == wood)
-		return get_wood_map(false);
-	if (t == tree)
-		return get_tree_map(false);
-	return (NULL);
-}
-
-t_normal_map	*erase_maps(t_normap_type t)
-{
-	if (t == cliff)
-		return get_cliff_map(true);
-	if (t == sand)
-		return get_sand_map(true);
-	if (t == terrain)
-		return get_terrain_map(true);
-	if (t == wood)
-		return get_wood_map(true);
-	if (t == tree)
-		return get_tree_map(true);
-	return (NULL);
 }
