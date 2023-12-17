7b41bf2357218a72e6b603cf08898ce8c651f986
srapin <suzanne.rapin@ulb.be>
2023-12-17 19:23:53 +0100

    invalud read

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:55:23 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/17 06:45:44 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 19:08:45 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	parse_and_create(int fd, t_parsing_data *data)
 			continue ;
 		}
 		sp_line = ft_split(line, ' ');
-		if (sp_line && sp_line[0] && sp_line[0] != '\n')
+		if (sp_line && sp_line[0] && sp_line[0][0] != '\n')
 			elem = add_obj(sp_line, data, line);
 		free(line);
 		free_tab(sp_line);
