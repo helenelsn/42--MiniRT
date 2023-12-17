dc5876c891be9a0f236703b38aac608af6b1380a
Helene Lesny <hlesny@student.42.fr>
2023-12-17 19:54:11 +0100

    modif create elem, avant mettait pas d'erreur de parsing pour b=wp

--- src/create_elem/material.c
+++ src/create_elem/material.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   material.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 19:17:45 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/17 19:13:49 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 19:52:13 by hlesny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,37 @@
 
 bool	set_checkerboard(t_material *mat, char *info)
 {
-	if (!info || (ft_strlen(info) < 3) ||  (!ft_strisequal(info, "ch") && !ft_strisequal(info, "ch\n")))
+	if (!info || (ft_strlen(info) < 2) ||  (!ft_strisequal(info, "ch") && !ft_strisequal(info, "ch\n")))
 		return (false);
 	mat->textures.checkered = true;
 	return (true);
 }
 
+bool 	ft_str_end_with(char *str, char *end)
+{
+	int	i;
+	int	j;
+	
+	if (!end || !str)
+		return (false);
+	i = ft_strlen(str) -1;
+	j = ft_strlen(end) -1;
+	while (i >= 0 && j >= 0)
+	{
+		if (str[i] != end[j])
+			return (false);
+		j--;
+		i--;
+	}
+	if (i == -1 && j >= 0)
+		return (false);
+	return (true);
+}
+
 bool	set_bump_map_type(t_material *mat, char *info)
 {
-	if ((ft_strlen(info) < 2) || !info || !ft_str_beggin_with(info, "b=") || ft_strlen(info) < 3
-		|| ft_strlen(info) > 4)
+	if ((ft_strlen(info) < 3) || !info || !ft_str_beggin_with(info, "b=") 
+		|| (ft_strlen(info) == 4 && !ft_str_end_with(info, "\n")) ) //|| ft_strlen(info) < 3 || ft_strlen(info) > 4)
 		return (false);
 	if (info[2] == 'c')
 		mat->textures.t = cliff;
