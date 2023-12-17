6516ddcc49077de2460c3da94f0d1a10848d96ed
Suzanne Rapin <suzanne.rapin@lilo.org>
2023-12-17 02:46:02 +0100

    norm

--- src/utils/change_base.c
+++ src/utils/change_base.c
@@ -3,55 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   change_base.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlesny <hlesny@student.42.fr>              +#+  +:+       +#+        */
+/*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 00:03:13 by srapin            #+#    #+#             */
-/*   Updated: 2023/12/14 16:05:24 by hlesny           ###   ########.fr       */
+/*   Updated: 2023/12/17 02:41:37 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/mini_rt.h"
 
-t_vec change_base_of_vec(t_vec v, t_vec trans, t_matrix *rot)
+t_vec	change_base_of_vec(t_vec v, t_vec trans, t_matrix *rot)
 {
-	t_vec ret;
-	t_matrix *tmp;
-	t_matrix *tmp2;
+	t_vec		ret;
+	t_matrix	*tmp;
+	t_matrix	*tmp2;
 
-	tmp = new_matrix_from_var_args(3, 1, v.x,v.y,v.z);	
-	// printf("--------------------------------------------\n");
-	
-	// print_mat(tmp);
+	tmp = new_matrix_from_var_args(3, 1, v.x, v.y, v.z);
 	tmp2 = matrix_product(rot, tmp);
-	// print_mat(tmp2);
 	del_mat(tmp);
 	ret.x = tmp2->matrix[0][0];
 	ret.y = tmp2->matrix[1][0];
 	ret.z = tmp2->matrix[2][0];
 	del_mat(tmp2);
 	ret = vect_addition(ret, trans);
-	return ret;
-	
+	return (ret);
 }
-
-// t_point change_base_of_point(t_point v, t_vec trans, t_matrix *rot)
-// {
-// 	t_vec ret;
-// 	t_point true_ret;
-// 	t_matrix *tmp;
-// 	t_matrix *tmp2;
-
-// 	tmp = new_matrix_from_var_args(3, 1, v.x,v.y,v.z);	
-// 	// printf("--------------------------------------------\n");
-	
-// 	print_mat(tmp);
-// 	tmp2 = matrix_product(rot, tmp);
-// 	print_mat(tmp2);
-// 	del_mat(tmp);
-// 	ret.x = tmp2->matrix[0][0];
-// 	ret.y = tmp2->matrix[1][0];
-// 	ret.z = tmp2->matrix[2][0];
-// 	ret = vect_addition(v, trans);
-// 	return ret;
-	
-// }
--- src/utils/fov.c
+++ src/utils/fov.c
@@ -6,19 +6,20 @@
 /*   By: srapin <srapin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 00:46:56 by srapin            #+#    #+#             */
-/*   Updated: 2023/10/19 00:48:03 by srapin           ###   ########.fr       */
+/*   Updated: 2023/12/17 02:42:16 by srapin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/mini_rt.h"
 
-int get_fov(char *str)
+int	get_fov(char *str)
 {
-    if (!str || !ft_strisint(str))
-        return (-1);
-    int nb;
-    nb = atoi(str);
-    if (0 > nb || 180 < nb)
-        return(-1);
-    return (nb);
+	int	nb;
+
+	if (!str || !ft_strisint(str))
+		return (-1);
+	nb = atoi(str);
+	if (0 > nb || 180 < nb)
+		return (-1);
+	return (nb);
 }
